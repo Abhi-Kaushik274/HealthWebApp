221b8e71888f930cf4878f0adcf0f5e5507d2da6
Abhi Kaushik <abhikaushik@Abhis-MacBook-Air.local>
2025-07-01 23:30:45 +0530

    BaseHref

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -10,7 +11,9 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename="/healthcareapp">
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
